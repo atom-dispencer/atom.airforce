b402bd9cf2f1ef4cbabdd7a90f4aa799ac890692
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-08-17 18:36:37 +0100

    Now it doesn't look atrocious

---
+++ www/index.js
@@ -0,0 +1,59 @@
+// iatom.uk
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+const centreX = (elem) => elem.offset().left + (elem.width() / 2)
+const centreY = (elem) => elem.offset().top + (elem.height() / 2)
+const centre = (elem) => { x = centreX(elem), y = centreY(elem) }
+
+// Blur intro as you scroll
+$(window).scroll(function () {
+    var BLUR_MODULUS = 50
+    var BLUR_MAX_PX = 250
+
+    var pixs = $(window).scrollTop() / BLUR_MODULUS
+    pixs = Math.min(pixs, BLUR_MAX_PX)
+    var newCss = {
+        "-webkit-filter": "blur(" + pixs + "px)",
+        "filter": "blur(" + pixs + "px)"
+    }
+    $(".intro").css(newCss)
+});
+
+// Blur left and right subheadings as you move
+$(window).mousemove(function (event) {
+    var shLeft = $('#subheading-left')
+    var shRight = $('#subheading-right')
+
+    var minX = shLeft.offset().left + (shLeft.width() / 2)
+    var maxX = shRight.offset().left + (shRight.width() / 2)
+    var mouseX = event.pageX
+    var mouseY = event.pageY
+    var width = maxX - minX
+
+    var clampMX = clamp(mouseX, minX, maxX)
+    var percent = (clampMX - minX) / (width)
+    var blurScale = Math.pow(percent, 3)
+
+    function shBlur(sh, scale) {
+        var BLUR_MAX_PX = 5
+        var blur = BLUR_MAX_PX * scale
+
+        var newCss = {
+            "-webkit-filter": "blur(" + blur + "px)",
+            "filter": "blur(" + blur + "px)"
+        }
+        sh.css(newCss)
+    }
+    function shTranslate(sh, scale) {
+        var tX = scale * 10
+        tX = Math.sqrt(Math.abs(scale)) * (scale < 0 ? -1 : 1)
+        var newCss = {
+            "transform": "translate(" + tX + "%)"
+        }
+        sh.css(newCss)
+    }
+    shBlur(shLeft, blurScale)
+    shBlur(shRight, 1 - blurScale)
+    shTranslate(shLeft, mouseX / $(window).width())
+    shTranslate(shRight, mouseX / $(window).width())
+});
