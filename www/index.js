ed896f52de9114eee7c82fcfb32b26f2171b4725
Adam <46679475+atom-dispencer@users.noreply.github.com>
2025-08-08 23:31:59 +0100

    Fun typing animation

--- www/index.js
+++ www/index.js
@@ -1,7 +1,18 @@
 console.log("Loading atom.airforce JavaScript...");
 
+const title_text = "atom.airforce"
+
 var setup_complete = false;
 var kute_script = document.createElement("script");
+var typing_index = 0;
+var title_element = null;
+
+var typing_interval = null;
+var intervals_since_flash = 0;
+var is_flashed = false;
+var title_current_text = "";
+var has_typo = 0;
+var typo_cooldown = 0;
 
 function lazy_setup()
 {
@@ -13,25 +24,107 @@ function lazy_setup()
   setup_complete = true;
   console.log("Finishing setup...");
 
+  // Start typing
+  title_element = document.getElementById("title");
+  typing_interval = window.setInterval(typing_effect, 100);
+
   // Start wave animation
   kute_script.src = "lib/kute.min.js";
-  kute_script.onload = () => window.setInterval(blend_waves, 5000);
+  kute_script.onload = () => {
+    blend_waves();
+    window.setInterval(blend_waves, 5000);
+  }
   document.head.appendChild(kute_script);
-  blend_waves()
+}
+
+function typing_effect()
+{
+  if (title_element.innerHTML.length == 0)
+  {
+    title_element.innerHTML = "_";
+  }
+
+  // Update flashing
+  if (intervals_since_flash++ > 5)
+  {
+    is_flashed = !is_flashed;
+    intervals_since_flash = 0;
+  }
+
+  // Handle typing / typos
+  var r = Math.random();
+  if (title_current_text.length < title_text.length || has_typo || typo_cooldown > 0)
+  {
+    // The typist is confused!
+    if (typo_cooldown > 0)
+    {
+      typo_cooldown--;
+    }
+    // Add a character
+    else if (r < 0.7 || has_typo)
+    {
+      if (has_typo)
+      {
+        title_current_text = title_current_text.substring(0, title_current_text.length - 1);
+        has_typo = false;
+        typo_cooldown = 0;
+      }
+      else
+      {
+        title_current_text += title_text.charAt(typing_index++);
+      }
+    }
+    // Add a typo
+    else if (r < 0.8 && title_current_text.length < title_text.length)
+    {
+      const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+      const index = Math.floor(Math.random() * characters.length);
+      title_current_text += characters.charAt(index);
+      has_typo = true;
+      typo_cooldown = Math.floor(5 * Math.random()) + 5;
+    }
+    // Pause 
+    else
+    {
+      // Do nothing: the typist has paused for thought.
+    }
+  }
+
+  // Set text
+  if (is_flashed)
+  {
+    title_element.innerHTML = title_current_text + "_";
+  }
+  else
+  {
+    title_element.innerHTML = title_current_text + " ";
+  }
 }
 
 function blend_waves()
 {
   console.log("Blending waves...");
 
-  const next_wave_index = Math.floor(9.99 * Math.random());
+  // Pick a random wave from 1-9
+  // Little bit of fiddling to get floor to work properly:
+  //   [0,1) -> 0
+  //   [1,2) -> 1
+  //   [9,9.99] -> 9
+  const next_wave_index = Math.floor(9.99 * Math.random()) + 1;
   const path = `resources/wave-${next_wave_index}.svg?=${Math.random()}`;
+
   fetch(path)
     .then(resource => resource.text())
     .then(svg_text => {
       const svg = new DOMParser().parseFromString(svg_text, "image/svg+xml");
       const svg_darker = svg.getElementById('darker');
       const svg_lighter = svg.getElementById('lighter');
+
+      if (svg_darker == null || svg_lighter == null)
+      {
+        console.log("uh oh why is that null?");
+      }
+
       const new_darker_path_data = svg_darker.getAttribute('d');
       const new_lighter_path_data = svg_lighter.getAttribute('d');
 
