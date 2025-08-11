b740a6796c684990adf669cecdf40426681a7bbe
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2025-08-11 15:40:34 +0000

    cron

--- certbot-renewal-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo -u docker-http bash -c 'cd /home/docker-http/atom.airforce && docker-compose down'
-
-sudo certbot renew --dry-run
-
-sudo -u docker-http bash -c 'cd /home/docker-http/atom.airforce && docker-compose down'
--- certbot-renewal.sh
+++ certbot-renewal.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo -u docker-http bash -c 'cd /home/docker-http/atom.airforce && docker-compose down'
+{
+  sudo -u docker-http bash -c 'cd /home/docker-http/atom.airforce && docker-compose down'
 
-sudo certbot renew
+  sudo certbot renew
 
-sudo -u docker-http bash -c 'cd /home/docker-http/atom.airforce && docker-compose down'
+  sudo -u docker-http bash -c 'cd /home/docker-http/atom.airforce && docker-compose down'
+} >> /home/docker-http/atom.airforce/certbot-cron.log 2>&1
